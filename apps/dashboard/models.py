0338d6c681adb94e53d04b484abc6ffaee55a50c
ghyoun <ghyoun@uw.edu>
2016-06-29 19:04:38 -0700

    added a single log out button

--- apps/dashboard/models.py
+++ apps/dashboard/models.py
@@ -3,3 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    details = models.TextField()
+    event_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
